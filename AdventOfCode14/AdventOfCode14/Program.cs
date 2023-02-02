3eb09d05dd5487bc7c14b5db0298b222e28d0cee
Alexandru Pupsa <alexandru.pupsa@tremend.com>
2023-02-02 16:25:25 +0200

    Solve day 14 - part 1

---
+++ AdventOfCode14/AdventOfCode14/Program.cs
@@ -0,0 +1,162 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+char[,] map;
+
+var rows = 0;
+var cols = 0;
+
+var sandEntryCol = 0;
+
+Setup();
+Part1();
+
+void Setup()
+{
+    var points = new List<(int, int)>();
+    foreach (var line in lines)
+    {
+        var splits = line.Split("->", StringSplitOptions.RemoveEmptyEntries);
+        var origin = GetPointFromPair(splits[0]);
+        for (var i = 1; i < splits.Length; i++)
+        {
+            var point = GetPointFromPair(splits[i]);
+            var fixedCoord = 0;
+            var movingCoordStart = 0;
+            var movingCoordEnd = 0;
+            var isCol = origin.Item1 == point.Item1;
+            if (isCol)
+            {
+                fixedCoord = origin.Item1;
+                movingCoordStart = Math.Min(origin.Item2, point.Item2);
+                movingCoordEnd = Math.Max(origin.Item2, point.Item2);
+            }
+            else
+            {
+                fixedCoord = origin.Item2;
+                movingCoordStart = Math.Min(origin.Item1, point.Item1);
+                movingCoordEnd = Math.Max(origin.Item1, point.Item1);
+            }
+            for (var j = movingCoordStart; j <= movingCoordEnd; j++)
+            {
+                if (isCol)
+                {
+                    points.Add((fixedCoord, j));
+                }
+                else
+                {
+                    points.Add((j, fixedCoord));
+                }
+            }
+            origin = point;
+        }
+    }
+
+    var maxRow = points.Max(x => x.Item2);
+    var minCol = points.Min(x => x.Item1);
+    var maxCol = points.Max(x => x.Item1);
+
+    rows = maxRow + 1;
+    cols = maxCol - minCol + 1;
+    sandEntryCol = 500 - minCol;
+
+    map = new char[rows, cols];
+
+    for (var i = 0; i < map.GetLength(0); i++)
+    {
+        for (var j = 0; j < map.GetLength(1); j++)
+        {
+            if (i == 0 && j == sandEntryCol)
+            {
+                map[i, j] = '+';
+            }
+            else
+            {
+                map[i, j] = '.';
+            }
+        }
+    }
+
+    foreach (var point in points)
+    {
+        map[point.Item2, point.Item1 - minCol] = '#';
+    }
+}
+
+void Part1()
+{
+    PrintMap();
+    var sandX = 0;
+    var sandY = sandEntryCol;
+    var ifInfiniteFall = false;
+    var countStillSand = 0;
+    while (!ifInfiniteFall)
+    {
+        if (map[sandX, sandY] == 'x')
+        {
+            map[sandX, sandY] = '.';
+        }
+        var possibleCoords = new List<(int, int)>
+        {
+            (sandX + 1, sandY),
+            (sandX + 1, sandY - 1),
+            (sandX + 1, sandY + 1)
+        };
+
+        var newX = -1;
+        var newY = -1;
+        foreach (var coord in possibleCoords)
+        {
+            if (coord.Item1 >= rows || coord.Item2 < 0 || coord.Item2 >= cols)
+            {
+                ifInfiniteFall = true;
+                break;
+            }
+            if (map[coord.Item1, coord.Item2] == '.')
+            {
+                newX = coord.Item1;
+                newY = coord.Item2;
+                break;
+            }
+        }
+        if (newX >= 0 && newY >= 0)
+        {
+            sandX = newX;
+            sandY = newY;
+            map[sandX, sandY] = 'x';
+        }
+        else if (!ifInfiniteFall)
+        {
+            map[sandX, sandY] = 'o';
+            countStillSand++;
+            sandX = 0;
+            sandY = sandEntryCol;
+        }
+        else
+        {
+            map[sandX, sandY] = '~';
+        }
+    }
+    PrintMap();
+    Console.WriteLine(countStillSand);
+}
+
+
+
+(int, int) GetPointFromPair(string input)
+{
+    var splits = input.Split(",");
+    return (int.Parse(splits[0]), int.Parse(splits[1]));
+}
+
+void PrintMap()
+{
+    Console.Clear();
+    for (var i = 0; i < map.GetLength(0); i++)
+    {
+        for (var j = 0; j < map.GetLength(1); j++)
+        {
+            Console.Write(map[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
