ba014aa78fe783bc6cbebdd4ce32a9c21bf44304
Alexandru Pupsa <alexandru.pupsa@tremend.com>
2023-02-06 12:44:26 +0200

    Refactor + try to solve day 15 - part 2

--- AdventOfCode15/AdventOfCode15/Program.cs
+++ AdventOfCode15/AdventOfCode15/Program.cs
@@ -2,12 +2,13 @@
 
 var lines = File.ReadAllLines("input.txt");
 
-var sensorBeaconPairs = new List<SignalBeacon>();
+var sensorBeaconPairs = new List<SensorBeacon>();
 
 var checkedY = 2000000;
+var MAX_SIZE = 4000000;
 
 Setup();
-Part1();
+Part2();
 
 void Setup()
 {
@@ -20,7 +21,7 @@ void Setup()
         var sY = int.Parse(splits[1].Substring(splits[1].IndexOf('=') + 1, indexOfSemiColumn - indexOfEquals - 1));
         var bX = int.Parse(splits[1].Substring(splits[1].LastIndexOf('=') + 1));
         var bY = int.Parse(splits[2].Substring(splits[2].LastIndexOf('=') + 1));
-        sensorBeaconPairs.Add(new SignalBeacon((sX, sY), (bX, bY)));
+        sensorBeaconPairs.Add(new SensorBeacon((sX, sY), (bX, bY)));
     }
 }
 
@@ -30,11 +31,11 @@ void Part1()
     var beaconsOnCheckedLine = new List<(int, int)>();
     foreach (var pair in sensorBeaconPairs)
     {
-        var startX = pair.SignalCoords.Item1 - pair.ManhattanDistance;
-        var endX = pair.SignalCoords.Item1 + pair.ManhattanDistance;
+        var startX = pair.SensorCoords.Item1 - pair.ManhattanDistance;
+        var endX = pair.SensorCoords.Item1 + pair.ManhattanDistance;
         for (var i = startX; i <= endX; i++)
         {
-            if (Utils.ManhattanDistance(pair.SignalCoords.Item1, pair.SignalCoords.Item2, i, checkedY) <= pair.ManhattanDistance)
+            if (Utils.ManhattanDistance(pair.SensorCoords.Item1, pair.SensorCoords.Item2, i, checkedY) <= pair.ManhattanDistance)
             {
                 noSensors.Add((i, checkedY));
             }
@@ -48,3 +49,53 @@ void Part1()
     Console.WriteLine(noSensors.Count);
 }
 
+void Part2()
+{
+    var noSensors = new List<(int, int)>();
+    var beaconsOnCheckedLine = new List<(int, int)>();
+    var foundX = 0;
+    var foundY = 0;
+
+    var pairCount = 0;
+    foreach (var pair in sensorBeaconPairs)
+    {
+        var startX = pair.SensorCoords.Item1 - pair.ManhattanDistance;
+        var endX = pair.SensorCoords.Item1 + pair.ManhattanDistance;
+        var startY = pair.SensorCoords.Item2 - pair.ManhattanDistance;
+        var endY = pair.SensorCoords.Item2 + pair.ManhattanDistance;
+        var step = 1;
+        var done = false;
+        for (var i = startX; i <= (endX + 1) / 2 && !done; i++, step++)
+        {
+            for (var j = startY; j <= (endY + 1) / 2 - step; j++)
+            {
+                var found = true;
+                foreach (var otherPair in sensorBeaconPairs.Where(x => x != pair))
+                {
+                    if (Utils.ManhattanDistance(otherPair.SensorCoords.Item1, otherPair.SensorCoords.Item2, i, j) <= otherPair.ManhattanDistance)
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    foundX = i;
+                    foundY = j;
+                    done = true;
+                    break;
+                }
+            }
+        }
+        if (done)
+        {
+            break;
+        }
+        pairCount++;
+        Console.WriteLine(pairCount);
+    }
+
+    Console.WriteLine(foundX);
+    Console.WriteLine(foundY);
+    Console.WriteLine(foundX * MAX_SIZE + foundY);
+}
---
+++ AdventOfCode15/AdventOfCode15/SensorBeacon.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode15
+{
+    public class SensorBeacon
+    {
+        public (int, int) SensorCoords { get; set; }
+        public (int, int) BeaconCoords { get; set; }
+        public int ManhattanDistance { get; set; }
+        public SensorBeacon((int, int) sensorCoords, (int, int) beaconCoords)
+        {
+            SensorCoords = sensorCoords;
+            BeaconCoords = beaconCoords;
+            ManhattanDistance = Utils.ManhattanDistance(sensorCoords.Item1, sensorCoords.Item2, beaconCoords.Item1, beaconCoords.Item2);
+        }
+    }
+}
